791f28a27d5bfb1cf4505fabfc37fdcf7324d474
aziz1sh1 <148104150+aziz1sh1@users.noreply.github.com>
2023-11-01 01:55:21 +0500

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,9 +1,14 @@
+import keyboard
 
 file = open('age.txt', 'w')
 print('This is age calculator !', file = file)
 
 print ("This pogram is used to calculate age\n")
+
+keyboard.press_and_release('enter')
 a = int (input("Enter current year\t") or "2023")
+keyboard.press_and_release('enter')
+
 b = int (input("enter you birth year\t") or "1981")
 
 if a == b:
@@ -21,8 +26,9 @@ print(age,"years")
 print ("Results depends on your input")
 
 file.close()
+keyboard.press_and_release('enter')
 input("Enter to exit")
-
+keyboard.press_and_release('enter')
 import os
 p = path = ("age.txt")
 os.system(path)
